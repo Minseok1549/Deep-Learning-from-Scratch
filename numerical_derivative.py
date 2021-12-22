73ca94eba6b5c125104921541cacc100a5100e54
Minseok1549 <72805319+Minseok1549@users.noreply.github.com>
2021-12-22 23:07:17 +0900

    Add files via upload
    Add Deep Neural Networks with numpy

---
+++ numerical_derivative.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def numerical_diff(f, x):
+    # f is function for diff
+    h = 1e-4
+    return (f(x+h) - f(x-h)) / 2*h
+
+def _numerical_gradient_1d(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.shape[0]):
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x) # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+
+        x[idx] = tmp_val # 값 복원
+
+    return grad
+
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_1d(f, x)
+
+        return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
